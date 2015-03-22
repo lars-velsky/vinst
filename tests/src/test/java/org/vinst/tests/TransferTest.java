3a7078427ec2c2d26d0110005a8157dd3f201dd9
lars-velsky <lars.velsky@gmail.com>
2015-03-22 23:21:30 +0300

    transfer test stub

---
+++ tests/src/test/java/org/vinst/tests/TransferTest.java
@@ -0,0 +1,15 @@
+package org.vinst.tests;
+
+import org.junit.Test;
+
+/**
+ * @author Lars Velsky
+ * @since 14/01/15
+ */
+public class TransferTest {
+
+    @Test
+    public void testSimpleTransfer(){
+        // todo
+    }
+}
